ac61247b563143a90ed8a3f5f9de4676c5e2c4a9
antonyadhiban <gtadhib@gmail.com>
2018-06-21 23:16:14 +0530

    Setup URL for main application

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -15,7 +15,25 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+# Use include() to add paths from the catalog application
+from django.conf.urls import include
+from django.urls import path
+#Add URL maps to redirect the base URL to our application
+from django.views.generic import RedirectView
+# Use static() to add url mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
+
+urlpatterns += [
+    path('catalog/', include('catalog.urls')),
+]
+
+urlpatterns += [
+    path('', RedirectView.as_view(url='/catalog/')),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
